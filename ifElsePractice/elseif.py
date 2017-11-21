370b482dc613c8f3b5d6eaf5aca0ad8baa0bf123
Jesus369 <31966603+Jesus369@users.noreply.github.com>
2017-11-21 12:48:52 -0600

    Update elseif.py

--- ifElsePractice/elseif.py
+++ ifElsePractice/elseif.py
@@ -32,7 +32,9 @@ else:
 
 
 print("\n\n")
-# Run to the grocery store
+
+
+# Run to the grocery store. Nesting if, elif, else statements
 response = "yes"
 
 print("""Evan: Can you go and buy some apples for my salad!?
@@ -45,19 +47,21 @@ demand = int(raw_input("Enter a number \t"))
 # If user input is greater than 4 the following code will run.
 if demand > 4:
 	print("*Drives home* Uh oh! too many! I think it was suppose to be 4.")
-	correct = raw_input("Was Jesus suppose to buy 4?")
+	correct = raw_input("Was Jesus suppose to buy 4?\t")				# Must prompt another code for the user to help him out tut thus starting a new if statement within an if statement.
 	if  correct.lower() == response.lower():
 		print("Well too much of something never killed anyone!")
 	elif correct.lower() != response.lower():
 		print("You're crazy! Shouldn't be asking you anyways!")
 #But if the user input is less than 4 then this code will run. 
+
 elif demand  < 4:
 	print("*Drives home* Uh oh! too little! I think it was suppose to be 4 right?")
-	correct = raw_input("Did jesus have to buy 4?")
+	correct = raw_input("Did jesus have to buy 4?\t")				# Same method as shown above
 	if correct.lower() == response.lower():
 		print("i'm in for it when i get home!!")
 	elif correct.lower() != response.lower():
 		print("Quit your jibber jabbering!")
+		
 # Otherwise if it is neither less than or greater than 4 and the user guess 4 the first time the following code will run.
 else:
 	print("Good thing I've got a great memory!")
