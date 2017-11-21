750cc5b4239ea5330c49826fefb336c5cb9e3a06
Jesus369 <31966603+Jesus369@users.noreply.github.com>
2017-11-21 12:45:47 -0600

    Update elseif.py

--- ifElsePractice/elseif.py
+++ ifElsePractice/elseif.py
@@ -1,3 +1,4 @@
+# Old enough to vote?
 name = raw_input("What is your name?")
 age = int(raw_input("How old are you?"))
 if age >= 18:
@@ -31,16 +32,17 @@ else:
 
 
 print("\n\n")
-
+# Run to the grocery store
 response = "yes"
 
-print("""Melissa: Babe can you go and buy some apples for my salad!?
-Jesus: How many do you need?
-Melissa: 4 should be enough
+print("""Evan: Can you go and buy some apples for my salad!?
+Steph: How many do you need?
+Evan: 4 should be enough
 *Drives to the grocery store*
-Jesus: Crap! How many apples did she say I needed to buy again?""")
+Steph: Crap! How many apples did he say I needed to buy again?""")
 demand = int(raw_input("Enter a number \t"))
 
+# If user input is greater than 4 the following code will run.
 if demand > 4:
 	print("*Drives home* Uh oh! too many! I think it was suppose to be 4.")
 	correct = raw_input("Was Jesus suppose to buy 4?")
@@ -48,15 +50,15 @@ if demand > 4:
 		print("Well too much of something never killed anyone!")
 	elif correct.lower() != response.lower():
 		print("You're crazy! Shouldn't be asking you anyways!")
-
+#But if the user input is less than 4 then this code will run. 
 elif demand  < 4:
 	print("*Drives home* Uh oh! too little! I think it was suppose to be 4 right?")
 	correct = raw_input("Did jesus have to buy 4?")
 	if correct.lower() == response.lower():
-		print("Now i'm screwed!!")
+		print("i'm in for it when i get home!!")
 	elif correct.lower() != response.lower():
 		print("Quit your jibber jabbering!")
-
+# Otherwise if it is neither less than or greater than 4 and the user guess 4 the first time the following code will run.
 else:
 	print("Good thing I've got a great memory!")
 		
