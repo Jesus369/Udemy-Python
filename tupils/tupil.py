87920d0d3cb9af2445241ca0187638579e7ea1b5
Jesus369 <31966603+Jesus369@users.noreply.github.com>
2017-11-26 15:30:43 -0600

    Update tupil.py

--- tupils/tupil.py
+++ tupils/tupil.py
@@ -1,14 +1,15 @@
 # Tupils which are similar to list. Tupils is an immutable object - They can't be changed easily changed like how a list can be.
 # Another note is that lists are meant to hold items of the same type only.
 # Where as tupils are intended to hold hetrogenius items. Explanation is that once an album is release, it's contents can not change.
+
 welcome = "Welcome to my Nightmate", "Alice Cooper", 1971
 bad = "Bad Company", "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lightning", "Metallica", 1984
 
-print(metallica) # Will print ["Ride the Lighting", "Metallica", 1984]
-print(metallica[0]) # Will prit Ride the Lightning [0] represents the first string in "metallica".
+print(metallica)        # Will print ["Ride the Lighting", "Metallica", 1984]
+print(metallica[0])     # Will prit Ride the Lightning [0] represents the first string in "metallica".
 print(metallica[1])
 print metallica[2]
 # Above is shown you calling the strings individually & below we print them all out together with a "for loop"
@@ -16,7 +17,7 @@ for i in metallica:
     print(i)
 # metallica[0] = "Master of puppets" <-- This will NOT change the string to Master of puppets.
 imelda = imelda[0], "Imelda May", imelda[2] # Go in order. When imelda[1], being the one you want to rename comes along. Instead of imelda[1], write the new name.
-print(imelda)   #prints "More Mayhem", "Imelda May", "2011".
+print(imelda)           #prints "More Mayhem", "Imelda May", "2011".
 
 print("=" * 40)
 
@@ -26,7 +27,7 @@ title, artist, date, track = data
 print(title)
 print(artist)
 print(date)
-print(track) # Calling the parent tupil will print all individual tupils inside of it.
+print(track)            # Calling the parent tupil will print all individual tupils inside of it.
 
 for song in track:
     track, title = song # Setting variables for the songs in track
@@ -34,7 +35,7 @@ for song in track:
 
 print("=" * 40)
 
-for song in track:  # With a parent tupil we can't go through the children individually, so we use a "for loop".
+for song in track:      # With a parent tupil we can't go through the children individually, so we use a "for loop".
     print(song)
 
 print("=" * 40)
